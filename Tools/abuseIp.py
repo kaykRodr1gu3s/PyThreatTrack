1a26feb1fe5d42ef0dffeaeb1626851f5f2e7fac
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2024-03-26 10:44:46 -0300

    abuseIp.py finished
    All the ips has been colected

--- Tools/abuseIp.py
+++ Tools/abuseIp.py
@@ -1,7 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-req = requests.get("https://www.abuseipdb.com/", headers={"User-agent": "Mozilla/5.0"})
+class Ips_class:
+    
+    def __init__(self) -> None:
+        self.base_url = 'https://www.abuseipdb.com/'
+        self.header = {"User-agent": "Mozilla/5.0"}
+        self.ip_list = []
 
-page_content = BeautifulSoup.find_all
-print(req.status_code)
+    
+    def abuseip_request(self) -> bytes:
+        
+        """
+        
+        This function, do the request to https://www.abuseipdb.com, and return the page content 
+        
+        """
+
+        req = requests.get(self.base_url, headers=self.header)
+        
+        return req.content
+
+
+    def getting_ip_list(self, page_content: bytes) -> list:
+
+        """
+        
+       This function takes the page content as an argument and parses the HTML from the page. It then uses regex to find the pattern and extract the IP addresses listed under 'Recently Reported IPs'.
+        
+        """
+
+
+        bs = BeautifulSoup(page_content, "html.parser")
+        ips = bs.find_all("a", href=re.compile(r'https://www.abuseipdb.com/check/(\d.)+'))
+        
+        for ip in ips:
+            self.ip_list.append(ip.text)
+
+        return self.ip_list
+    
