00bcbde5a558d132b1a15295cfd9cac3a23ced5d
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2024-03-28 21:22:15 -0300

    Add the CSVprocessor
    The CSVprocessor , help to left the code more readable

--- abuse_api.py
+++ abuse_api.py
@@ -1,22 +1,39 @@
 import requests
-from Tools.csv_creators import abuseip_csv_creator
+from Tools.csv_creators import CSVprocessor
+from Tools import ips
 
-url = 'https://api.abuseipdb.com/api/v2/check'
+class abuseipapi:
+    def __init__(self, API) -> None:
+        self.endpoint = "https://api.abuseipdb.com/api/v2/check"
+        self.header = {
+                'Accept': 'application/json',
+                'Key': API
+                        } 
+        
+    def search(self, ip_list):
+        datas_to_csv = []
+        for ip in ip_list:
+            
+            query = {
+            'ipAddress': ip,
+            'maxAgeInDays': '90'
+            }
+            req = requests.get(url=self.endpoint, headers=self.header, params=query)
+            data = req.json()
+            data = data['data']
+            datas_to_csv.append(data)
+        print(datas_to_csv)
+        return datas_to_csv
 
-headers = {
-    'Accept': 'application/json',
-    'Key': 'api'
-}
-querystring = {
-    'ipAddress': '95.214.53.99',
-    'maxAgeInDays': '90'
-}
 
-req = requests.request(method="GET" , url=url, headers=headers, params=querystring)
-test = req.json()
-test = test['data']
+list_ip = ips.Ips_class()
+abuseip_page_content = list_ip.abuseip_request()
+all_ips = list_ip.getting_ip_list(abuseip_page_content)
 
 
-a = abuseip_csv_creator.abuseip()
-a.data([test])
+abuseip = abuseipapi("API KEY")
+datas_to_csv = abuseip.search(all_ips)
 
+csv = CSVprocessor.abuseip("abuseIP.csv")
+dir_name = csv.verifing_directory()
+csv.appending_datas(dir_name,datas_to_csv)
